d627786e0fdb0cf575e572e7c33f46d8902ba661
linjinux <linjinux@126.com>
2019-07-17 16:18:48 +0800

    change jobmodule share scan

--- app/appmodule/jobmodule/shareftpscan.py
+++ app/appmodule/jobmodule/shareftpscan.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import csv
-import sys
 a="can.csv"
-with open(a,'rt') as input_file:
+with open(a,'rt',encoding='utf-16') as input_file:
     filereader = csv.reader(input_file,delimiter='\t')
+    id(filereader)
     for row_list in filereader:
+        print(row_list)
         if not row_list[6].strip()=='' or not row_list[8].strip()=='': 
             with open('test.csv','a+',encoding='utf-8',newline="") as f:
-                f_csv = csv.writer(f)
                 f_csv.writerow(row_list)
